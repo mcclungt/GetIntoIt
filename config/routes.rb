a36661c9cd95c6ecb9d7daa07199f02b8394b0f1
Tim <timothyjmcclung@gmail.com>
2014-10-28 14:34:23 -0400

    basic forms, not working

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'user#new'
+  root 'user#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :user
 
+  get 'login' => 'sessions#new', as: :login
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy', as: :logout
   # Example resource route with options:
   #   resources :products do
   #     member do
