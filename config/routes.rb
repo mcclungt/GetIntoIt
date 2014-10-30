753cabcdb4171656b86bd74a1765961164c290d6
Tim <timothyjmcclung@gmail.com>
2014-10-30 15:31:53 -0400

    fixed post routing error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  # root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,10 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :users
-
-  get 'login' => 'sessions#new', as: :login
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy', as: :logout
+  
+  post 'users/new' => 'sessions#create'
   # Example resource route with options:
   #   resources :products do
   #     member do
