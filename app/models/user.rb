83e8a10d7a2a26512143c772f388a12638898525
Tim <timothyjmcclung@gmail.com>
2014-11-10 16:22:58 -0500

    merging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 	validates_confirmation_of :password
-	validates :username, uniqueness: true, presence: true
-	validates :password, length: { minimum: 6}
+	validates :username, uniqueness: true
 	has_secure_password
+	
 	has_many :articles
 
 	before_save { self.username = username.downcase }
